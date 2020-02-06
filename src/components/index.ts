d3c0b1b25779a455939335eaa6061c3a1355fd23
nga.le <levanga1125@gmail.com>
2020-02-06 19:20:02 +0700

    update source

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,12 @@ import ControlChatBot from "./ControlChatBot";
 import ControlCircle from "./ControlCircle";
 import ControlImage from "./ControlImage";
 import ControlText from "./ControlText";
+import ControlHeader from "./ControlHeader";
+
 export {
     ControlChatBot,
     ControlCircle,
     ControlImage,
-    ControlText
+    ControlText,
+    ControlHeader
 };
